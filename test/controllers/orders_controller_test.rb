d012bd345cfdf0857a5486b4367ed032c35af918
Stefflan00 <stefan.maier09@gmail.com>
2014-07-11 13:02:13 +0200

    basics setup before styling frontend

---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionController::TestCase
+  test "should get status" do
+    get :status
+    assert_response :success
+  end
+
+  test "should get checkout" do
+    get :checkout
+    assert_response :success
+  end
+
+  test "should get payment" do
+    get :payment
+    assert_response :success
+  end
+
+  test "should get confirmation" do
+    get :confirmation
+    assert_response :success
+  end
+
+end
